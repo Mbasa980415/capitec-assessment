a78718c007fe1818de54e37b971c8b0e69a77ac0
Megabyte <Megabyte980415@gmail.com>
2025-07-18 00:42:47 +0200

    PART-03: Updated baseTest fixture with db setup

--- src/fixtures/baseTest.fixture.ts
+++ src/fixtures/baseTest.fixture.ts
@@ -1,14 +1,26 @@
 import { test as baseTest, } from '@playwright/test';
 import { OnlineStore } from '@online-store/page/onlineStore.page'; 
+import { DatabaseHelper } from '@database/page/databaseHelper.page';
+import client, { dbConnection } from '@utils/dbConnection.util';
 
 const test = baseTest.extend<{
   onlineStore: OnlineStore;
+  databaseHelper: DatabaseHelper;
 }>({
 
   onlineStore: async ({ page }, use) => {
     const store = new OnlineStore(page);
     await use(store);
   },
+
+  databaseHelper: async ({}, use) =>{
+
+    await dbConnection();
+    const helper = new DatabaseHelper(client);
+
+    await use(helper);
+    await client.end();
+  }
 
 });
 
