2e82437dcc1ef4f7cb9622443187fbfc7de6fbe7
Megabyte <Megabyte980415@gmail.com>
2025-07-18 00:42:13 +0200

    PART-03: Database Helper file

---
+++ src/component/database/page/databaseHelper.page.ts
@@ -0,0 +1,42 @@
+import type { Client } from 'pg';
+
+type UserInput = {
+  title: string;
+  completed: boolean;
+  user_id: number;
+};
+
+export class DatabaseHelper {
+
+  constructor(private client: Client) {}
+
+
+  async createRecord(input: UserInput) {
+    const record = await this.client.query(
+      'INSERT INTO tasks (title, completed, user_id) VALUES ($1, $2, $3) RETURNING *;',
+      [input.title, input.completed, input.user_id]);
+      
+    return record.rows[0];
+  }
+
+  async retrieveRecord(id: number) {
+    const record = await this.client.query('SELECT * FROM tasks WHERE id = $1', [id]);
+    return record.rows[0];
+  }
+
+  async updateRecord(id: number, column: string, value: string | number | boolean) {
+ 
+    const columnNames= ['title', 'completed', 'user_id']; //SECURITY: Prevention of malicious SQL query injection!
+    if (!columnNames.includes(column)) {
+      throw new Error('Invalid column name');
+    }
+
+    const record = await this.client.query(
+      `UPDATE tasks SET ${column} = $1 WHERE id = $2 RETURNING *;`,
+      [value, id]);
+      
+    return record.rows[0];
+  }
+
+
+}
