cef73c9e1da69e2e53eeffbae63f9bdf6ab135ab
Megabyte <Megabyte980415@gmail.com>
2025-07-17 13:09:54 +0200

    PART-01: Updated online store test

--- src/component/online-store/tests/purchaseProduct.test.ts
+++ src/component/online-store/tests/purchaseProduct.test.ts
@@ -3,7 +3,7 @@ import test, {expect} from '@fixtures/baseTest.fixture'
 type ProductName = 'backpack' | 'bike-light' | 'fleece-jacket' | 'onesie';
 const productNames: ProductName[] = ['backpack', 'bike-light', 'fleece-jacket', 'onesie'];
 
-test.describe("Online shopping @UI", () =>{
+test.describe("Online shopping E2E @ui", () =>{
 
     for(const productName of productNames){
         test(`validate Purchase of ${productName} from online store`, async({onlineStore})=>{
